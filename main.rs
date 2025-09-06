120088173e30560a6114793b3232bdea25b12912
realdanvanth <realdanvanth@gmail.com>
2025-09-06 23:39:18 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,22 +1,26 @@
-#![allow(unused_imports)]
-use crossterm::{ExecutableCommand,terminal,cursor};
-use crossterm::style::{Print,SetBackgroundColor,SetForegroundColor,Color};
-use std::io::{stdout,Write};
 #[macro_export]
 macro_rules! eterm{
     (clear) => {stdout().execute(terminal::Clear(terminal::ClearType::All)).unwrap()}; 
     (flush) => {stdout().flush().unwrap()};
     (bold)  => {stdout().execute(SetAttribute(Attribute::Bold))};
+    (size)  => {terminal::size().unwrap()};
+    (raw)   => {enable_raw_mode().unwrap()};
+    (unraw) => {disable_raw_mode().unwrap()};
     (underline) => {stdout().execute(SetAttribute(Attribute::underline))};
-    (move($s:expr,$s1:expr))=> {stdout().execute(cursor::MoveTo($s,$s1)).unwrap()};
-    (color(fg,$s:ident)) =>{stdout().execute(SetForegroundColor(Color::$s)).unwrap()};
-    (color(bg,$s:ident)) =>{stdout().execute(SetBackgroundColor(Color::$s)).unwrap()};
-    (print($s:literal)) => {stdout().execute(Print($s.to_string())).unwrap()};
+    (move($x:expr,$y:expr))=> {stdout().execute(cursor::MoveTo($y,$x)).unwrap()};
+    (color(fg,$color:ident)) =>{stdout().execute(SetForegroundColor(Color::$color)).unwrap()};
+    (color(bg,$color:ident)) =>{stdout().execute(SetBackgroundColor(Color::$color)).unwrap()};
+    (print($string:literal)) => {stdout().execute(Print($string.to_string())).unwrap()};
+    (poll($time:expr)) => {poll(Duration::from_millis($time)).unwrap()};
+    //(keyInput) => 
 }
-fn main(){
-    eterm!(clear);
-    eterm!(color(fg,Red));
-    eterm!(move(0,0));
-    println!("hello");
-    eterm!(flush);
+macro_rules! input{
+    ($t:ty) => {{
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input.trim().parse::<$t>().unwrap()
+    }};
+}
+macro_rules! string{
+    ($s:literal)=> {($s).to_string();};
 }
