7667d94790d24013f92f88f3561ac47b90115c18
realdanvanth <realdanvanth@gmail.com>
2025-09-10 19:07:06 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,18 +1,24 @@
-#[macro_export]
 macro_rules! eterm{
     (clear) => {stdout().execute(terminal::Clear(terminal::ClearType::All)).unwrap()}; 
+    (clearline($x:expr,$y:expr))=>{
+        eterm!(move($x,0));
+        execute!(stdout(),Print(" ".repeat(($y).into()))).unwrap();
+    };
     (flush) => {stdout().flush().unwrap()};
     (bold)  => {stdout().execute(SetAttribute(Attribute::Bold))};
     (size)  => {terminal::size().unwrap()};
     (raw)   => {enable_raw_mode().unwrap()};
     (unraw) => {disable_raw_mode().unwrap()};
+    (blink) => {stdout().execute(cursor::EnableBlinking)};
+    (dblink) => {stdout().execute(cursor::DisableBlinking)};
+    (hide) => {stdout().execute(cursor::Hide).unwrap()};
+    (show) => {stdout().execute(cursor::Show).unwrap()};
     (underline) => {stdout().execute(SetAttribute(Attribute::underline))};
     (move($x:expr,$y:expr))=> {stdout().execute(cursor::MoveTo($y,$x)).unwrap()};
     (color(fg,$color:ident)) =>{stdout().execute(SetForegroundColor(Color::$color)).unwrap()};
     (color(bg,$color:ident)) =>{stdout().execute(SetBackgroundColor(Color::$color)).unwrap()};
     (print($string:literal)) => {stdout().execute(Print($string.to_string())).unwrap()};
     (poll($time:expr)) => {poll(Duration::from_millis($time)).unwrap()};
-    //(keyInput) => 
 }
 macro_rules! input{
     ($t:ty) => {{
@@ -21,6 +27,8 @@ macro_rules! input{
         input.trim().parse::<$t>().unwrap()
     }};
 }
-macro_rules! string{
-    ($s:literal)=> {($s).to_string();};
+macro_rules! whichtype{
+    ($i:ident) => {
+        std::any::type_name_of_val(&$i)
+    }
 }
